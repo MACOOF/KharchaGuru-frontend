db1ded8ad7fccd8b5f49de310c8408ac31f86cda
sarthak mayani <mayanisarthak@gmail.com>
2025-02-26 18:23:32 +0530

    made somechanges in server actions

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,6 +3,7 @@ import { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import Google from "next-auth/providers/google"
 import { getUserByEmail } from "./data/user"
+import { SigninSchema } from "./lib"
 // import { getSchemas } from "@/lib/index"
 // import { getTranslations } from "next-intl/server"
 
